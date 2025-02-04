008eb7f3dca027f9ae1761228796fbeda2c9cc95
Pratik Biswas <04biswaspratik@gmail.com>
2025-02-04 11:32:46 +0530

    home page UI is updated

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -4,6 +4,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import Baseline from "@/components/Baseline/Baseline";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +25,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground min-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground min-h-screen flex flex-col items-center justify-center`}
       >
         <ToastContainer />
         <Header />
+        <Baseline />
         <main>{children}</main>
         {/* <Footer /> */}
       </body>
---
+++ client/src/components/Baseline/Baseline.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Baseline = () => {
+  return (
+    <div className="flex items-center w-fit justify-center gap-10 fixed bottom-0 z-50 bg-blue-500 px-10 py-3 rounded-t-full">
+      <div>Call icon +91 7001316356</div>
+      <div>Mail icon messi10.pratikbiswas@gmail.com</div>
+      <div>Get Appointment</div>
+      <div>Food Analyser</div>
+      <div>Chat Assistant</div>
+    </div>
+  );
+};
+
+export default Baseline;
