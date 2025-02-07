38a7ffd7143402611220e02f4113774296b04211
Pratik Biswas <04biswaspratik@gmail.com>
2025-02-07 12:17:22 +0530

    drug response is updated

--- client/src/components/DrugResponse/index.jsx
+++ client/src/components/DrugResponse/index.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-
+import Image from "next/image";
 export default function DrugResponsePage() {
   const [formData, setFormData] = useState({
     drug_name: "",
@@ -49,145 +49,181 @@ export default function DrugResponsePage() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-3xl mx-auto">
-        <h2 className="text-3xl font-extrabold text-gray-900 text-center mb-8">
-          Fill Your Information Here
-        </h2>
+    <section className="flex flex-col relative w-[1300px] h-[300px] my-10 mb-[35rem]  items-center justify-center ">
+      <div className="absolute inset-0">
+        <Image
+          src="/diseaseMain.png"
+          alt="Commercial Real Estate"
+          layout="fill"
+          objectFit="cover"
+          className="rounded-3xl drop-shadow-lg opacity-80"
+        />
+      </div>
 
-        <form
-          onSubmit={handleSubmit}
-          className="bg-white shadow-md rounded-lg p-6"
-        >
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            {/* Drug Name */}
-            <div>
-              <label className="block text-gray-700">Drug Name:</label>
-              <input
-                type="text"
-                name="drug_name"
-                value={formData.drug_name}
-                onChange={handleChange}
-                required
-                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500"
-              />
-            </div>
+      {/* Text Content */}
+      <div className="relative flex flex-col items-center justify-center gap-5 text-black px-6">
+        <h1 className="text-3xl sm:text-5xl  text-center  font-montserrat">
+          Find Your Alternative Drug
+        </h1>
+        <p className="font-lato text-gray-800 text-center max-w-3xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          deserunt neque labore distinctio nostrum eos doloribus consectetur
+          fugit ea numquam, ex, hic exercitationem, quod voluptates aliquid
+          expedita vitae
+        </p>
+      </div>
+      {/* Form Box */}
+      <div className="absolute left-1/2 transform -translate-x-1/2 top-[240px]  w-full max-w-5xl bg-blue-50 shadow-xl rounded-lg p-6">
+        <h3 className="text-lg font-medium font-mono text-gray-900 mb-4 text-center">
+          -- Fill Your Medication Information Here --
+        </h3>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div className="grid grid-cols-1  gap-4">
+            <div className="grid grid-cols-4 items-center justify-center gap-2">
+              {/* Drug Name */}
+              <div>
+                <input
+                  type="text"
+                  name="drug_name"
+                  value={formData.drug_name}
+                  placeholder="Drug Name"
+                  onChange={handleChange}
+                  required
+                  className="w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500 font-lato placeholder:font-sans sm:text-sm"
+                />
+              </div>
 
-            {/* Gender */}
-            <div>
-              <label className="block text-gray-700">Gender:</label>
-              <select
-                name="gender"
-                value={formData.gender}
-                onChange={handleChange}
-                required
-                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500"
-              >
-                <option value="">Select Gender</option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-                <option value="Gay">Gay</option>
-                <option value="Lesbian">Lesbian</option>
-                <option value="Transgender">Transgender</option>
-              </select>
-            </div>
+              {/* Gender */}
+              <div>
+                <select
+                  name="gender"
+                  value={formData.gender}
+                  onChange={handleChange}
+                  required
+                  className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500 font-lato sm:text-sm"
+                >
+                  <option value="">Select Gender</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                  <option value="Gay">Gay</option>
+                  <option value="Lesbian">Lesbian</option>
+                  <option value="Transgender">Transgender</option>
+                </select>
+              </div>
 
-            {/* Age */}
-            <div>
-              <label className="block text-gray-700">Age:</label>
-              <input
-                type="number"
-                name="age"
-                value={formData.age}
-                onChange={handleChange}
-                required
-                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500"
-              />
-            </div>
+              {/* Age */}
+              <div>
+                <input
+                  type="number"
+                  name="age"
+                  value={formData.age}
+                  onChange={handleChange}
+                  required
+                  placeholder="Age"
+                  className="w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500 font-lato placeholder:font-sans sm:text-sm"
+                />
+              </div>
 
-            {/* BMI */}
-            <div>
-              <label className="block text-gray-700">BMI:</label>
-              <input
-                type="number"
-                step="0.1"
-                name="bmi"
-                value={formData.bmi}
-                onChange={handleChange}
-                required
-                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500"
-              />
+              {/* BMI */}
+              <div>
+                <input
+                  type="number"
+                  step="0.1"
+                  name="bmi"
+                  placeholder="BMI"
+                  value={formData.bmi}
+                  onChange={handleChange}
+                  required
+                  className="w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500 font-lato placeholder:font-sans sm:text-sm"
+                />
+              </div>
             </div>
 
             {/* Past Medical Conditions */}
-            <div className="sm:col-span-2">
-              <label className="block text-gray-700">
-                Past Medical Conditions:
-              </label>
+            <div className=" col-span-2">
               <textarea
                 name="past_medical_conditions"
                 value={formData.past_medical_conditions}
+                placeholder="Past Medical Conditions"
                 onChange={handleChange}
                 required
                 rows="3"
-                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500"
+                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500 font-lato placeholder:font-sans sm:text-sm max-h-28"
               ></textarea>
             </div>
 
             {/* Existing Health Conditions */}
-            <div className="sm:col-span-2">
-              <label className="block text-gray-700">
-                Existing Health Conditions:
-              </label>
+            <div className=" col-span-2">
               <textarea
                 name="existing_health_conditions"
                 value={formData.existing_health_conditions}
+                placeholder="Existing Health Conditions"
                 onChange={handleChange}
                 required
                 rows="3"
-                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500"
+                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500 font-lato placeholder:font-sans sm:text-sm max-h-28"
               ></textarea>
             </div>
 
             {/* Allergies */}
-            <div className="sm:col-span-2">
-              <label className="block text-gray-700">Allergies:</label>
+            <div className=" col-span-2">
               <textarea
                 name="allergies"
                 value={formData.allergies}
+                placeholder="Allergies"
                 onChange={handleChange}
                 required
                 rows="3"
-                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500"
+                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500 font-lato placeholder:font-sans sm:text-sm max-h-28"
               ></textarea>
             </div>
           </div>
 
           {/* Submit Button */}
-          <div className="mt-6 text-center">
+          <div className="flex items-center justify-center">
             <button
               type="submit"
-              disabled={isLoading}
-              className="w-full sm:w-auto px-4 py-2 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:outline-none disabled:opacity-50"
+              disabled={
+                isLoading ||
+                !formData.age ||
+                !formData.allergies ||
+                !formData.bmi ||
+                !formData.drug_name ||
+                !formData.existing_health_conditions ||
+                !formData.gender ||
+                !formData.past_medical_conditions
+              }
+              className={`w-fit py-2 px-4 rounded-md shadow-md font-medium font-ubuntu transition-all duration-500 transform ${
+                isLoading ||
+                !formData.age ||
+                !formData.allergies ||
+                !formData.bmi ||
+                !formData.drug_name ||
+                !formData.existing_health_conditions ||
+                !formData.gender ||
+                !formData.past_medical_conditions
+                  ? "bg-gray-300 text-gray-500 cursor-not-allowed"
+                  : "bg-blue-500 text-white hover:bg-blue-700 hover:scale-110 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              }`}
             >
               {isLoading ? "Submitting..." : "Submit"}
             </button>
           </div>
         </form>
-
-        {/* Response Section */}
-        {error && (
-          <div className="mt-6 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
-            <strong className="font-bold">Error: </strong> {error}
-          </div>
-        )}
-
-        {response && (
-          <div className="mt-6 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative">
-            <strong className="font-bold">Response: </strong> {response.message}
-          </div>
-        )}
       </div>
-    </div>
+
+      {/* Response Section */}
+      {error && (
+        <div className="mt-6 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
+          <strong className="font-bold">Error: </strong> {error}
+        </div>
+      )}
+
+      {response && (
+        <div className="mt-6 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative">
+          <strong className="font-bold">Response: </strong> {response.message}
+        </div>
+      )}
+    </section>
   );
 }
